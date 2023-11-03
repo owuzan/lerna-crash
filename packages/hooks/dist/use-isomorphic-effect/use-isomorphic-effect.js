7675dbb69850efb03e0f544201cafddb1d17047c
Oğuzhan Yılmaz <ooguzhanyilmazz41@gmail.com>
2023-11-04 02:51:04 +0300

    remove dist

--- packages/hooks/dist/use-isomorphic-effect/use-isomorphic-effect.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useIsomorphicEffect = void 0;
-const react_1 = require("react");
-// useLayoutEffect will show warning if used during ssr, e.g. with Next.js
-// useIsomorphicEffect removes it by replacing useLayoutEffect with useEffect during ssr
-exports.useIsomorphicEffect = typeof document !== 'undefined' ? react_1.useLayoutEffect : react_1.useEffect;
--- packages/hooks/dist/use-local-storage/use-local-storage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useLocalStorage = void 0;
-const create_storage_1 = require("./create-storage");
-function useLocalStorage(props) {
-    return (0, create_storage_1.createStorage)('localStorage', 'use-local-storage')(props);
-}
-exports.useLocalStorage = useLocalStorage;
