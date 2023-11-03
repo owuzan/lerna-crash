ded61589745afd75f09a46fe0049f52daee9fbe3
Oğuzhan Yılmaz <ooguzhanyilmazz41@gmail.com>
2023-11-04 02:51:55 +0300

    remove dist

--- packages/hooks/dist/use-scroll-into-view/utils/ease-in-out-quad.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.easeInOutQuad = void 0;
-const easeInOutQuad = (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t);
-exports.easeInOutQuad = easeInOutQuad;
